e3fd15004a4bb4297b4b673310f4fb9b359f2fec
Tara Dehdari <tara.dehdari@gmail.com>
2020-10-02 16:14:13 -0700

    server.js and added routes folder

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require("express");
+
+var app = express();
+
+var PORT = process.env.PORT || 3000;
+
+//setting up express app to handle data
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+//routes
+
+//listen
+app.listen(PORT, function(){
+    console.log("app is listening on PORT: " + PORT);
+});
