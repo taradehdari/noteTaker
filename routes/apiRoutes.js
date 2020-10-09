d3e2e2ca03e517e441fa39f46d1be37e377c9c03
Tara Dehdari <tara.dehdari@gmail.com>
2020-10-08 20:04:49 -0700

    trouble shooting got no saved notes section on notes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,21 +1,25 @@
 //connecting routes to data
 var data = require("../data/data");
 
+console.log("data: ", data);
+
 module.exports = function (app) {
 
     //GET
-    app.get("/api/journals", function (req, res) {
+    app.get("/api/notes", function (req, res) {
         res.json(data);
+        console.log("data in app.get(/api/journals): ", data);
     });
 
     //POST
-    app.post("/api/journals", function (req, res) {
+    app.post("/api/notes", function (req, res) {
         data.push(req.body);
+        console.log("data in app.post(/api/journals): ", data);
         res.json("saved");
       });
 
     //DELETE
-    app.delete("/api/journals/:index", function(req, res) {
+    app.delete("/api/notes/:index", function(req, res) {
         var elem = parseINT(req.params.index);
         var tempJournal = [];
         for (var i=0; i < data.length; i++) {
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -11,7 +11,7 @@ module.exports = function(app) {
         res.sendFile(path.join(__dirname, "../public/assts/css/styles.css"));
     });
 
-    //home page
+    //goes to home page
     app.get("*", function(req, res) {
         res.sendFile(path.join(__dirname, "../public/index.html"));
     });
